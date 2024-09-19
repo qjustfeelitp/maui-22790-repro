48a59c0ff211d2cf9359ceab450e39927e524df0
Pavel Štáfl <stafl@stormware.cz>
2024-09-19 13:23:07 +0200

    automated navigation included visual studio stack

--- src/Maui22790/MainPage.xaml.cs
+++ src/Maui22790/MainPage.xaml.cs
@@ -7,6 +7,15 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
+    /// <inheritdoc />
+    protected override async void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+
+        await Task.Delay(TimeSpan.FromMilliseconds(100));
+        await this.Navigation.PushAsync(new SecondPage(), true);
+    }
+
     private async void Navigate(object sender, EventArgs e)
     {
         await this.Navigation.PushAsync(new SecondPage(), true);
--- src/Maui22790/SecondPage.xaml.cs
+++ src/Maui22790/SecondPage.xaml.cs
@@ -7,6 +7,15 @@ public partial class SecondPage : ContentPage
 		InitializeComponent();
 	}
 
+    /// <inheritdoc />
+    protected override async void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+
+        await Task.Delay(TimeSpan.FromMilliseconds(100));
+        await this.Navigation.PopAsync();
+    }
+
     private async void Navigate(object sender, EventArgs e)
     {
         await this.Navigation.PopAsync();
